90ad1c7ca8218555c2fdb25509ae29b6435b7d35
Daniel Donaldson <daniel.donaldson@definition6.com>
2018-06-09 12:48:36 -0400

    further experimentation stroke fun

--- events.js
+++ events.js
@@ -4,6 +4,7 @@ regionA.mouseover((e) => {
     console.log(e.target);
     e.target.style.opacity = .5;
     e.target.style.fill = '#326789';
+    e.target.style.stroke = '#000000';
 });
 
 regionA.mouseout( (e) => {
